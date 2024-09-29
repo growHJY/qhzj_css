250528bff288f43f20396128167ea87b83800bef
何纪乙 <hejiyi@hejiyideMacBook-Air.local>
2024-09-29 15:03:40 +0800

    在config.py中配置了mysql的信息

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 TOKEN_SECRET = "qhzj_token"
 SERVER_HOST = "http://192.168.10.8:5001"
+
+MYSQL_HOST = "localhost"
+MYSQL_PORT = 3306
+MYSQL_DB = "kf_sys"
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "12345678"
--- utils/mysql_operation.py
+++ utils/mysql_operation.py
@@ -1,15 +1,16 @@
 import pymysql
 
+from flask import current_app
+
 
 class MysqlOperation:
     def __init__(self):
         self.conn = None
-
-        self.host = 'localhost'
-        self.port = 3306
-        self.user = 'root'
-        self.password = '12345678'
-        self.database = 'kf_sys'
+        self.host = current_app.config['MYSQL_HOST']
+        self.port = current_app.config['MYSQL_PORT']
+        self.user = current_app.config['MYSQL_USER']
+        self.password = current_app.config['MYSQL_PASSWORD']
+        self.database = current_app.config['MYSQL_DB']
 
     def connect(self):
         self.conn = pymysql.connect(host=self.host, port=self.port, user=self.user, password=self.password,
