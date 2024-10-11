63229e0d9b0673cf4fafee5c6091695f02dfce0e
hejiyi <xiaohejy0417@gmail.com>
2024-10-11 16:02:07 +0800

    完善文件下载逻辑

--- routers/download.py
+++ routers/download.py
@@ -3,6 +3,7 @@
 from flask import Blueprint, jsonify, request, send_file, current_app
 from src.utils.jwt_util import get_user_from_token
 from src.pojo.response import Response as R
+from src.enum_tool.file_type_enum import FileTypeEnum
 
 import os
 
@@ -11,25 +12,31 @@ download_blueprint = Blueprint("download", __name__, url_prefix="/api/download")
 STATIC_AUDIO_PATH = os.path.join(os.getcwd(), "resources", "audio")
 
 
-@download_blueprint.route("/get_audio", methods=["GET"])
+@download_blueprint.route("", methods=["GET"])
 def get_audio():
     token = request.headers.get("Authorization")
     user = get_user_from_token(token, current_app.config["TOKEN_SECRET"])
-    file_name = request.args.get("audio_code")
-    # 用于分辨是从文字聊天还是从语音聊天发送的请求
-    # 0: 语音 1: 文字
-    get_type = request.args.get("type")
+    file_type = request.args.get("file_type")
     file = None
-    try:
-        if get_type == "0":
-            file = send_file(os.path.join(STATIC_AUDIO_PATH, "uploads", "file_" + user.uphone, file_name + ".mp3"),
-                             as_attachment=True)
-        elif get_type == "1":
-            file = send_file(
-                os.path.join(STATIC_AUDIO_PATH, "text2audio", "file_" + user.uphone, file_name + ".mp3"),
-                as_attachment=True)
+    if file_type == FileTypeEnum.AUDIO:  # 音频下载
+        file_name = request.args.get("audio_code")
+        # 用于分辨是从文字聊天还是从语音聊天发送的请求
+        # 0: 语音 1: 文字
+        get_type = request.args.get("type")
 
-    except FileNotFoundError:
-        pass
-        return jsonify(R(404, "未找到文件", None).to_dict())
+        try:
+            if get_type == "0":
+                file = send_file(os.path.join(STATIC_AUDIO_PATH, "uploads", "file_" + user.uphone, file_name + ".mp3"),
+                                 as_attachment=True)
+            elif get_type == "1":
+                file = send_file(
+                    os.path.join(STATIC_AUDIO_PATH, "text2audio", "file_" + user.uphone, file_name + ".mp3"),
+                    as_attachment=True)
+
+        except FileNotFoundError:
+            pass
+            return jsonify(R(404, "未找到文件", None).to_dict())  #
+    elif file_type == FileTypeEnum.IMAGE:  # 图片下载
+        filename = request.args.get("filename")
+        file = send_file(os.path.join(os.getcwd(), "resources","article_image", filename))
     return file
