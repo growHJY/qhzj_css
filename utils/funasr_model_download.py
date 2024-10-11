6813fe57d2c25b465a50300bade5346bdf0de76d
hejiyi <xiaohejy0417@gmail.com>
2024-10-11 11:57:49 +0800

    完善音频转文字模型下载

--- utils/funasr_model_download.py
+++ utils/funasr_model_download.py
@@ -1,2 +1,10 @@
+import os
 from modelscope import snapshot_download
-model_dir = snapshot_download('iic/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch', cache_dir="./model")
+
+model_path = os.path.join(os.path.dirname(os.getcwd()), "model")
+
+if not os.path.exists(model_path):
+    os.mkdir(model_path)
+
+model_dir = snapshot_download('iic/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
+                              cache_dir=model_path)
